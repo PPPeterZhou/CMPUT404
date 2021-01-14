7c34902174c66edc6442aa6edceeac34b3000a20
PPPeterZhou <44284614+PPPeterZhou@users.noreply.github.com>
2021-01-13 18:44:03 -0700

    3rd commit

--- lab1.py
+++ lab1.py
@@ -9,64 +9,10 @@ print(requests.__version__)
 
 # curl
 
-# step 1
-url = "http://google.com/"
-
-cmd = "curl " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-
-# step 2
-cmd = "curl -i " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-res = requests.get(url)
-print(res.status_code)
-
-# step 3
-cmd = "curl -iL " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-
-# step 4
-url = "https://www.google.com/teapot"
-
-cmd = "curl -i " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-res = requests.get(url)
-print(res.status_code)
-
-cmd = "curl -iL " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-res = requests.get(url)
-print(res.status_code)
-
-cmd = "curl " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-
 # step 5
-url = "http://google.com/"
-res = requests.get(url)
+google = requests.get('https://www.google.com/')
+print(google)
 
-# step 6
-
-url = "https://webdocs.cs.ualberta.ca/~hindle1/1.py"
-cmd = "curl -i " + url
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-
-# step 7
-cmd = 'curl -i -X POST -d "X=Y" ' + url 
-print("\nCommand:", cmd, "\n")
-os.system(cmd)
-
-'''
-git init
-git commit -m "lab1"
-git branch -M main
-git remote add origin https://github.com/PPPeterZhou/CMPUT404.git
-git push -u origin main 
-'''
+# step 10
+lab1 = requests.get("https://raw.githubusercontent.com/PPPeterZhou/CMPUT404/main/lab1.py")
+print(lab1.text)
